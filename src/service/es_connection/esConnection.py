456b20d084ed3a842a1a236bdbd210285244bd82
smfaurri <fabian.urrigshardt@stud.uni-due.de>
2022-05-04 17:36:26 +0200

    example implementation of esConnection. fake_serviceCommands for testing

---
+++ src/service/es_connection/esConnection.py
@@ -0,0 +1,40 @@
+import paho.mqtt.subscribe as subscribe
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import time
+from fake_serviceCommands import fake_serviceCommands
+
+def on_message_service(client, userdata, message):
+	print("Nachricht erhalten: " + str(message.payload))
+	
+	#todo: Message interpretieren. 
+	
+	wantedModel = "Model 1"
+	nodeId = 1	
+	
+	elastic_node = esConnection(nodeId)
+	model = elastic_node.connectForSearch(wantedModel)
+	elastic_node.serveModel(model)
+
+def waitForElasticNode():
+	subscribe.callback(on_message_service, "/service", hostname="broker.hivemq.com")
+
+
+class esConnection():
+	_nodeId=1 #Node braucht eine eindeutige id
+	 
+	def __init__(self, nodeId):	
+		self._nodeId = nodeId
+	
+	def connectForSearch(self, modelName:str)->str:		#todo:bytes statt string
+		commands = fake_serviceCommands()
+		model = commands.search(modelName)
+		return model
+		
+	def serveModel(self, model:str):	
+		topic = "/"+str(self._nodeId)
+		print("topic="+topic)
+		publish.single(topic, payload=model, hostname="broker.hivemq.com")
+
+if __name__=="__main__":
+	waitForElasticNode()
