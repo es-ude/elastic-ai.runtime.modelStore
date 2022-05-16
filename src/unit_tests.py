8c9fe15a5f705f3c3549a100028c136e7fc1b2e8
smfaurri <fabian.urrigshardt@stud.uni-due.de>
2022-05-16 17:43:51 +0200

    seperated integration and unit tests

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -1,18 +1,12 @@
 import unittest
 import service
 
-
-
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 suite.addTests(loader.loadTestsFromModule(service.tests.test_client_connection))
-suite.addTests(loader.loadTestsFromModule(service.tests.integration_test_client_connection))
-
 suite.addTests(loader.loadTestsFromModule(service.tests.test_request_handler))
-suite.addTests(loader.loadTestsFromModule(service.tests.integration_test_request_handler))
 
-suite.addTests(loader.loadTestsFromModule(service.tests.integration_test_mlflow_store_connection))
 
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
