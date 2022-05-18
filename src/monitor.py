8ba50da9cbc47f95d66a92f22038011ba4d6dde8
smfaurri <fabian.urrigshardt@stud.uni-due.de>
2022-05-18 15:01:44 +0200

    monitor

---
+++ src/monitor.py
@@ -0,0 +1,11 @@
+import service
+
+if __name__=="__main__": 
+	storeConnection = service.store_connection.MLflowStoreConnection("http://localhost:5000")
+	print("initialized storeConnection")
+	serviceCommands = service.service_commands.serviceCommands(storeConnection)
+	print("initialized serviceCommands")
+	requestHandler = service.request_handler.requestHandler(serviceCommands)	
+	print("initialized esManager")
+	requestHandler.waitForElasticNode()
+
