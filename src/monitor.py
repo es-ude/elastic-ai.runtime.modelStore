2a3ffa5a44dabe3c930fbee7c068db13cec0da6b
smfaurri <fabian.urrigshardt@stud.uni-due.de>
2022-05-21 09:59:00 +0200

    Monitor is a class

--- src/monitor.py
+++ src/monitor.py
@@ -1,10 +1,18 @@
 import service
 
+class Monitor:
+	def __init__(self):
+		pass
+	
+	def run(self):
+		storeConnection = service.store_connection.MLflowStoreConnection("http://localhost:5000")
+		print("initialized storeConnection")
+		serviceCommands = service.service_commands.ServiceCommands(storeConnection)
+		print("initialized serviceCommands")
+		requestHandler = service.request_handler.RequestHandler(serviceCommands)	
+		print("initialized request handler")
+		requestHandler.wait_for_elastic_node()
+
 if __name__=="__main__": 
-	storeConnection = service.store_connection.MLflowStoreConnection("http://localhost:5000")
-	print("initialized storeConnection")
-	serviceCommands = service.service_commands.ServiceCommands(storeConnection)
-	print("initialized serviceCommands")
-	requestHandler = service.request_handler.RequestHandler(serviceCommands)	
-	print("initialized request handler")
-	requestHandler.wait_for_elastic_node()
+	monitor = Monitor()
+	monitor.run()
